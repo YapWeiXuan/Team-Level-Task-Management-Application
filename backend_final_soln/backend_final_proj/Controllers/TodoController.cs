3b2d602e302e067d66d9c12dc436bf0ba722a31c
YapWeiXuan <xpecture@gmail.com>
2024-10-21 03:20:38 +0800

    Committed_backend

---
+++ backend_final_soln/backend_final_proj/Controllers/TodoController.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using System;
+using backend_final_proj.Input;
+
+namespace backend_final_proj.Controllers
+{
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public TodoController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        [HttpPost("delete_tasks")]
+        public JsonResult delete_tasks([FromForm] DELETE_Input_Format delete_input)
+        {
+            //string query = "select * from dbo.all_tasks";
+
+            string query = "delete from dbo.all_tasks" +
+                " where manager=@manager " +
+                "and manager_email=@manager_email " +
+                "and task_user=@task_user " +
+                "and task_title=@task_title " +
+                "and task_description=@task_description;";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+            SqlDataReader myReader;
+            using (SqlConnection conn = new SqlConnection(SqlDataSource))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@manager", delete_input.Manager);
+                    cmd.Parameters.AddWithValue("@manager_email", delete_input.Manager_email);
+                    cmd.Parameters.AddWithValue("@task_user", delete_input.Task_user);
+                    cmd.Parameters.AddWithValue("@task_title", delete_input.Task_title);
+                    cmd.Parameters.AddWithValue("@task_description", delete_input.Task_description);
+                    //Console.WriteLine( cmd.ToString());
+                    myReader = cmd.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+
+            }
+            return new JsonResult("Deleted Successfully!");
+        }
+
+        [HttpPost("add_tasks")]
+        public JsonResult add_tasks([FromForm] GET_Input_Format get_input)
+        {
+            //string query = "select * from dbo.all_tasks";
+
+            string query = "insert into dbo.all_tasks (manager,manager_email,task_user,task_title,task_description,time_created,due_date) " +
+                "values (@manager," +
+                "@manager_email," +
+                "@task_user," +
+                "@task_title," +
+                "@task_description," +
+                "@time_created," +
+                "@due_date);";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+            SqlDataReader myReader;
+            using (SqlConnection conn = new SqlConnection(SqlDataSource))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("@manager", get_input.Manager);
+                    cmd.Parameters.AddWithValue("@manager_email", get_input.Manager_email);
+                    cmd.Parameters.AddWithValue("@task_user", get_input.Task_user);
+                    cmd.Parameters.AddWithValue("@task_title", get_input.Task_title);
+                    cmd.Parameters.AddWithValue("@task_description", get_input.Task_description);
+                    cmd.Parameters.AddWithValue("@time_created", get_input.Time_created);
+                    cmd.Parameters.AddWithValue("@due_date", get_input.Due_date);
+
+                    myReader = cmd.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+                
+            }
+            return new JsonResult("Added Successfully!");
+        }
+
+        [HttpGet("get_tasks")]
+        public JsonResult get_tasks()
+        {
+            string query = "select * from dbo.all_tasks";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+            SqlDataReader myReader;
+            using (SqlConnection conn = new SqlConnection(SqlDataSource))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    myReader = cmd.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpGet("get_dist_users")]
+        public JsonResult get_dist_users()
+        {
+            string query = "select DISTINCT task_user from dbo.all_tasks";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
+            SqlDataReader myReader;
+            using (SqlConnection conn = new SqlConnection(SqlDataSource))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    myReader = cmd.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                }
+
+            }
+            return new JsonResult(table);
+        }
+
+
+
+    }
+}
---
+++ backend_final_soln/backend_final_proj/Input/Input_Format.cs
@@ -0,0 +1,221 @@
+﻿namespace backend_final_proj.Input
+{
+    public class GET_Input_Format
+    {
+        private string? manager;
+        private string? manager_email;
+        private string? task_user;
+        private string? task_title;
+        private string? task_description;
+        private DateTime time_created;
+        private DateTime due_date;
+
+        public string Manager
+        {
+            get
+            {
+                return this.manager;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.manager = " ";
+                }
+                else { this.manager = value; }
+
+            }
+
+        }
+        public string Manager_email
+        {
+            get
+            {
+                return this.manager_email;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.manager_email = " ";
+                }
+                else { this.manager_email = value; }
+            }
+
+        }
+        public string Task_user
+        {
+            get
+            {
+                return this.task_user;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_user = " ";
+                }
+                else { this.task_user = value; }
+            }
+
+        }
+        public string Task_title
+        {
+            get
+            {
+                return this.task_title;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_title = " ";
+                }
+                else { this.task_title = value; }
+            }
+
+        }
+        public string Task_description
+        {
+            get
+            {
+                return this.task_description;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_description = " ";
+                }
+                else { this.task_description = value; }
+            }
+
+        }
+        public DateTime Time_created
+        {
+            get
+            {
+                return this.time_created;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    DateTime localDate = DateTime.Now;
+                    this.time_created = localDate;
+                }
+                else { this.time_created = value; }
+            }
+
+        }
+        public DateTime Due_date
+        {
+            get
+            {
+                return this.due_date;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    DateTime localDate = DateTime.Now;
+                    this.due_date = localDate;
+                }
+                else { this.due_date = value; }
+            }
+
+        }
+
+    }
+    public class DELETE_Input_Format
+    {
+        private string? manager;
+        private string? manager_email;
+        private string? task_user;
+        private string? task_title;
+        private string? task_description;
+
+        public string Manager
+        {
+            get
+            {
+                return this.manager;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.manager = " ";
+                }
+                else { this.manager = value; }
+
+            }
+
+        }
+        public string Manager_email
+        {
+            get
+            {
+                return this.manager_email;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.manager_email = " ";
+                }
+                else { this.manager_email = value; }
+            }
+
+        }
+        public string Task_user
+        {
+            get
+            {
+                return this.task_user;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_user = " ";
+                }
+                else { this.task_user = value; }
+            }
+
+        }
+        public string Task_title
+        {
+            get
+            {
+                return this.task_title;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_title = " ";
+                }
+                else { this.task_title = value; }
+            }
+
+        }
+        public string Task_description
+        {
+            get
+            {
+                return this.task_description;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    this.task_description = " ";
+                }
+                else { this.task_description = value; }
+            }
+
+        }
+
+    }
+}
