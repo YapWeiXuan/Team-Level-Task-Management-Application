0e47201249ab98892f1cd664db7c2b82add8cac4
YapWeiXuan <125413695+YapWeiXuan@users.noreply.github.com>
2024-11-12 04:26:20 +0800

    Added Database Model and DbContext class files

---
+++ backend_final_soln/backend_final_proj/Models/AllTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace backend_final_proj.Models;
+
+public class AllTask
+{
+    [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+    public long? Id { get; set; }
+
+    public string? Manager { get; set; }
+
+    public string? ManagerEmail { get; set; }
+
+    public string? TaskUser { get; set; }
+
+    public string? TaskTitle { get; set; }
+
+    public string? TaskDescription { get; set; }
+
+    public DateTime? TimeCreated { get; set; }
+
+    public DateTime? DueDate { get; set; }
+}
---
+++ backend_final_soln/backend_final_proj/Models/TaskmanagementDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend_final_proj.Models;
+
+public partial class TaskmanagementDbContext : DbContext
+{
+    public TaskmanagementDbContext()
+    {
+    }
+
+    public TaskmanagementDbContext(DbContextOptions<TaskmanagementDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<AllTask> AllTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:AZURE_SQL_CONNECTIONSTRING");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<AllTask>(entity =>
+        {
+            entity
+                .HasKey(e => e.Id);
+                entity.ToTable("all_tasks");
+
+            entity.Property(e => e.DueDate)
+                .HasColumnType("datetime")
+                .HasColumnName("due_date");
+            entity.Property(e => e.Id)
+                .ValueGeneratedOnAdd()
+                .HasColumnName("id");
+            entity.Property(e => e.Manager).HasColumnName("manager");
+            entity.Property(e => e.ManagerEmail).HasColumnName("manager_email");
+            entity.Property(e => e.TaskDescription).HasColumnName("task_description");
+            entity.Property(e => e.TaskTitle).HasColumnName("task_title");
+            entity.Property(e => e.TaskUser).HasColumnName("task_user");
+            entity.Property(e => e.TimeCreated)
+                .HasColumnType("datetime")
+                .HasColumnName("time_created");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
